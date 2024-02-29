eb661bb277105fbee79829793f0a2fc993e036a2
John Hayes-reed <john.hayes-reed@gds13332.lan>
2024-02-29 12:33:31 +0000

    database, domain, and compose stack configuration changes

--- config/initializers/allowed_domains.rb
+++ config/initializers/allowed_domains.rb
@@ -1,13 +1,6 @@
 ALLOWED_DOMAINS = [
-  'justice.gov.uk',
-  'digital.justice.gov.uk',
-  'cica.gov.uk',
-  'ccrc.gov.uk',
-  'judicialappointments.gov.uk',
-  'judicialombudsman.gov.uk',
-  'ospt.gov.uk',
-  'gov.sscl.com',
-  'hmcts.net'
+  'cabinet-office.gov.uk',
+  'digital.cabinet-office.gov.uk'
 ].freeze
 
 Rails.application.config.allowed_domains = ALLOWED_DOMAINS
