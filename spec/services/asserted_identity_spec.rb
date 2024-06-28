afc9a5fc51b69bdde06389c8f799c6adabf2e40d
Александар Симић <alex@yaml.farm>
2024-06-28 14:33:17 +0100

    Removed Google & Azure auth relevant tests
    It looks like the app used Azure & Google to allow the users to auth
    against. I'm not aware that this will be used going forward, so I'm
    removing these tests.

--- spec/services/asserted_identity_spec.rb
+++ spec/services/asserted_identity_spec.rb
@@ -29,57 +29,5 @@ describe AssertedIdentity do
         end
       end
     end
-
-    context 'when google connection' do
-      let(:user_info) do
-        {
-          'info' => {
-            'uuid' => "google-oauth2|#{SecureRandom.uuid}",
-            'provider' => 'auth0',
-            'name' => 'Vanya Hargreeves',
-            'nickname' => 'vanya.hargreeves',
-            'email' => 'vanya.hargreeves@umbrella-academy.com'
-          }
-        }
-      end
-
-      it 'it correctly formats the name' do
-        expect(subject.from_auth0_userinfo(user_info).name).to eq('Vanya Hargreeves')
-      end
-    end
-
-    context 'when azure connection' do
-      let(:user_info) do
-        {
-          'info' => {
-            'uuid' => "waad|#{SecureRandom.uuid}",
-            'provider' => 'auth0',
-            'name' => 'Hargreeves, Fei',
-            'nickname' => 'Fei.Hargreeves',
-            'email' => 'Fei.Hargreeves@sparrow-academy.com'
-          }
-        }
-      end
-
-      it 'correctly formats the name' do
-        expect(subject.from_auth0_userinfo(user_info).name).to eq('Fei Hargreeves')
-      end
-    end
-
-    context 'when there is a number in the name' do
-      let(:user_info) do
-        {
-          'info' => {
-            'uuid' => "google-oauth2|#{SecureRandom.uuid}",
-            'provider' => 'auth0',
-            'email' => 'Rey.Mysterio619@hurricanrana.com'
-          }
-        }
-      end
-
-      it 'removes numbers from the name' do
-        expect(subject.from_auth0_userinfo(user_info).name).to eq('Rey Mysterio')
-      end
-    end
   end
 end
