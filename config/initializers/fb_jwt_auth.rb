2421971c36e3f18688bd5456b5320748fcf0fa75
Александар Симић <alex@yaml.farm>
2024-10-11 16:58:35 +0100

    Escape the env var content ENCODED_PRIVATE_KEY
    Because the value `\n` was escaped as `\\n`, so it would break the
    usage for JWT.
    
    This is probably because the env var is kept in AWS Secrets Manager,
    then read from there and populated as an env var within the Kubernetes
    deployment env var.

--- config/initializers/fb_jwt_auth.rb
+++ config/initializers/fb_jwt_auth.rb
@@ -1,5 +1,7 @@
+encoded_private_key = ENV['ENCODED_PRIVATE_KEY']
+
 Fb::Jwt::Auth.configure do |config|
   config.issuer = 'editor'
   config.namespace = "formbuilder-saas-#{ENV['PLATFORM_ENV']}"
-  config.encoded_private_key = ENV['ENCODED_PRIVATE_KEY']
+  config.encoded_private_key = encoded_private_key.gsub('\\n', "\n") unless encoded_private_key.nil?
 end
