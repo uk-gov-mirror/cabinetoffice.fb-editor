9e8929eeabe35090b30078feba040643069a414a
Александар Симић <alex@yaml.farm>
2024-10-11 17:00:48 +0100

    Use explicit names
    Using the set name, which will match the pod name and the namespace
    name in Kubernetes

--- config/initializers/fb_jwt_auth.rb
+++ config/initializers/fb_jwt_auth.rb
@@ -1,7 +1,8 @@
+
 encoded_private_key = ENV['ENCODED_PRIVATE_KEY']
 
 Fb::Jwt::Auth.configure do |config|
-  config.issuer = 'editor'
-  config.namespace = "formbuilder-saas-#{ENV['PLATFORM_ENV']}"
+  config.issuer = 'form-builder-editor'
+  config.namespace = 'form-builder'
   config.encoded_private_key = encoded_private_key.gsub('\\n', "\n") unless encoded_private_key.nil?
 end
